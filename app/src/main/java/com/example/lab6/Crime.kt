8e06695d2c0205274ad9714cd2803ad372066b69
zdrvzbl <sanya2003ogorodov@gmail.com>
2024-11-25 22:43:56 +0700

    feature: add Crime model

---
+++ app/src/main/java/com/example/lab6/Crime.kt
@@ -0,0 +1,10 @@
+package com.example.lab6
+
+import java.util.Date
+import java.util.UUID
+
+data class Crime(
+    val id: UUID = UUID.randomUUID(),
+    var title: String = "",
+    var date: Date = Date(),
+    var isSolved: Boolean = false)
